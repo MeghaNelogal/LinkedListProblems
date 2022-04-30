26d581f1b365b3cf9b4cebaa2c8a89de998abd21
Your Name <you@example.com>
2022-04-30 09:08:30 +0530

    [Megha] Add: Creating Linked List Is Done

---
+++ LinkedListProblems/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using LinkedListProblems;
+public class Program
+{
+    public static void Main(String[] args)
+    {
+        LinkedList list = new LinkedList();
+        list.Add(56);
+        list.Add(30);
+        list.Add(70);
+        list.Display();
+
+    }
+}
