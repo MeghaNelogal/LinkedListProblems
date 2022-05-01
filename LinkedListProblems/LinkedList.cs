c504448006c12e9edfcc4b96b5d82c9db3d1571b
Your Name <you@example.com>
2022-05-01 13:59:26 +0530

    [Megha] Add: Data Added Between The Linked List

--- LinkedListProblems/LinkedList.cs
+++ LinkedListProblems/LinkedList.cs
@@ -25,7 +25,7 @@ namespace LinkedListProblems
                 }
                 temp.next = node;
             }
-            Console.WriteLine("{0} Inserted into Linked List", node.data);
+            Console.WriteLine("{0} Inserted into Linked List\n", node.data);
         }
         public void Display()
         {
@@ -43,10 +43,10 @@ namespace LinkedListProblems
         }
         public void AddInReverseOrder(int data)
         {
-            Node newNode = new Node(data);
+            Node newNode = new Node(data); 
             if (head == null)
             {
-                head = newNode;
+                head = newNode; 
             }
             else
             {
@@ -54,6 +54,55 @@ namespace LinkedListProblems
                 head = newNode;
                 head.next = temp;
             }
+            Display();
+        }
+        public int Search(int value)
+        {
+            Node node = this.head;
+            int count = 0;
+            while (node != null)
+            {
+
+                if (node.data == value)
+                {
+                    return count;
+                }
+                node = node.next;
+                count++;
+            }
+            return count;
+        }
+
+        internal Node InsertAtParticularPosition(int position, int data)
+        {
+            Node newestNode = new Node(data);
+            if (this.head == null)
+            {
+                return newestNode;
+            }
+            if (position == 0)
+            {
+                newestNode.next = this.head;
+                this.head = newestNode;
+                return this.head;
+            }
+            Node prev = null;
+            Node current = this.head;
+            int count = 0;
+            while (current != null && count < position)
+            {
+                prev = current;
+                current = current.next;
+                count++;
+            }
+            newestNode.next = prev.next;
+            prev.next = newestNode;
+            return this.head;
         }
     }
 }
+
+
+
+
+
--- LinkedListProblems/Program.cs
+++ LinkedListProblems/Program.cs
@@ -7,11 +7,10 @@ public class Program
         Console.WriteLine("Welcome to Linked List Program");
         LinkedList list = new LinkedList();
         list.Add(56);
-        list.Add(30);
-        list.Add(70);
+        list.Add(70);     
+        int num = list.Search(56);
+        list.InsertAtParticularPosition(num + 1, 30);
         list.Display();
-        list.AddInReverseOrder(70);
-        list.Display();
-
     }
+
 }
