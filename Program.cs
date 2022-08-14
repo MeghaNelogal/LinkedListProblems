61dd1b973caa210137e5bcf8a8dde353103dac11
Your Name <you@example.com>
2022-08-14 14:07:55 +0530

    [Megha] Add: Added UC2

--- Program.cs
+++ Program.cs
@@ -7,11 +7,9 @@
             Console.WriteLine("Welcome To Linked List Problems");
 
             LinkedList list = new LinkedList();
+            list.Add(70);
             list.Add(56);
             list.Add(30);
-            list.Add(70);
-            list.Display();
-            list.AddInReverseOrder(70);
             list.Display();
         }
     }
