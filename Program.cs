88c8dee8204a990fc482b12c4ed84c14cfc06d87
Your Name <you@example.com>
2022-08-14 15:05:44 +0530

    [Megha] Add: Added UC9

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@
 
            
             bool end = true;
-            Console.WriteLine("Enter Option\n1.Add Data\n2.Reverse Data\n3.InsertData\n4.RemoveFirstElement\n5.RemoveLastElement\n6.Search Node\n7.InsertNewData\n8.End Of Program ");
+            Console.WriteLine("\n1.Add Data\n2.Reverse Data\n3.InsertData\n4.RemoveFirstElement\n5.RemoveLastElement\n6.Search Node\n7.InsertNewData\n8.DeleteNodeAtParticularPosition\n9.End Of Program ");
             while (end)
             {
                 LinkedList list = new LinkedList();
@@ -64,6 +64,17 @@
                         list.Display();
                         break;
                     case 8:
+                        list.Add(56);
+                        list.Add(30);
+                        list.Add(70);
+                        int node = list.Search(30);
+                        list.InsertAtParticularPosition(node + 1, 40);
+                        list.Display();
+                        Console.WriteLine("\n\nElement Delete SuccessFully");
+                        list.DeleteNodeAtParticularPosition(node + 1);
+                        list.Display();
+                        break;
+                    case 9:
                         end = false;
                         Console.WriteLine("Program Is Ended.");
                         break;
